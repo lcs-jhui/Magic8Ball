2a6a291f0d8207100376c51915c9ee37337a4395
Justin Hui <jhui@lcs.on.ca>
2023-10-24 20:07:47 -0400

    Initial Commit

---
+++ Magic8Ball/Magic8BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8BallApp.swift
+//  Magic8Ball
+//
+//  Created by Justin Hui on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Magic8BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
